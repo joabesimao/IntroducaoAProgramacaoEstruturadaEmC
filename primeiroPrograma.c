fcb361588c938bb64f7ef0263579758efe94af32
joabesimao <simaojoabecosta@gmail.com>
2022-03-16 18:56:27 -0300

    meu primeiro Programa em c.

---
+++ primeiroPrograma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("**************************\n");
+    printf("* Bem vindo ao nosso jogo * \n");
+    printf("**************************\n");
+
+    int numeroSecreto = 42;
+
+    int chute;
+
+    printf("qual o seu chute?\n");
+    scanf("%d",&chute);
+    printf("seu chute foi %d\n",chute);
+
+}
