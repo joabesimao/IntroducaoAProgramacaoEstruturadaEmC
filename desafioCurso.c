924bae08d3ba248962237f22b789bf85831d7eab
joabesimao <simaojoabecosta@gmail.com>
2022-03-21 15:39:40 -0300

    criar desafioCursoEstruturaRepeticao

--- desafioCurso.c
+++ desafioCurso.c
@@ -1,4 +1,4 @@
-/* #include <stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 
 int main(){
@@ -16,37 +16,5 @@ int main(){
     printf("o maior numero lido = %d\n",maior);
     return 0;
 
-} */
+} 
 
-#include <stdio.h>
-#include<stdlib.h>
-
-int main(){
-
-    int cont;
-    float salario,media,soma,maior;
-    maior =0;
-    soma =0;
-
-    for(cont =1;cont<=10;cont++){
-        printf("digite o seu salario:");
-        scanf("%f",&salario);
-
-        soma = soma+salario;
-
-        if(salario > maior){
-            maior = salario;
-        }
-    }
-
-    media = soma/5;
-
-    printf("o maior salario e = %.2f \n",maior);
-
-    printf("a media salarial e = %.2f \n",media);
-
-    return 0;
-
-
-
-}
